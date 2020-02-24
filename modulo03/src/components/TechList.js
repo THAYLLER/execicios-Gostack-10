5dd589d1e162bc980206f4e90da68e16a600f2c5
thayller <thayllervilelacintra@yahoo.com.br>
2020-02-24 09:33:15 -0300

    modulo 03 do gotack

---
+++ modulo03/src/components/TechList.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+
+import TechItem from './TechItem';
+
+class TechList extends Component{
+  static defaultProps = {
+    tech: 'Oculto',
+  };
+
+  state = {
+    newTech: '',
+    techs: [
+      'Node.js',
+      'ReactJS',
+      'React Native'
+    ]
+  };
+
+  handleInoutChange = e => {
+    this.setState({ newTech: e.target.value });
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState({ 
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: ''
+    });
+  }
+
+  handleDelete = (tech) =>  {
+    this.setState({ techs: this.state.techs.filter( t => t !== tech)})
+  }
+  render() {
+    return(
+      <>
+        <form onSubmit={this.handleSubmit}>
+          <ul>
+            {this.state.techs.map(tech => (
+              <TechItem 
+                key={tech} 
+                tech={tech} 
+                onDelete={() => this.handleDelete(tech)} 
+              />
+            ))}
+          </ul>
+          <input 
+            type="text" 
+            onChange={this.handleInoutChange}
+            value={this.state.newTech}
+          />
+          <button type="submit">Enviar</button>
+        </form>
+      </>
+    );
+  }
+}
+
+export default TechList;
